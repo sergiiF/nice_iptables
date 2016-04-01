627d74322692c65c6c6c5780c4c5216a3acc8f2d
sergiiF <framin@ukr.net>
2016-04-01 16:16:04 -0700

    Update setup.py
    Have to migrate to PBR with test requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ setuptools.setup(name='nice_iptables',
                  version='0.1.1',
                  author='skashaba',
                  scripts=['bin/nice_iptables'],
-                 install_requires=["nose", "flake8", "coverage"],
                  packages=setuptools.find_packages(exclude=['bin']))
 
